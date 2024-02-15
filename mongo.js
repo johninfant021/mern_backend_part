485bd812482cfe0bda7f502d9c32ddccbfb4629c
johninfant021 <127079706+johninfant021@users.noreply.github.com>
2024-02-15 10:07:05 +0530

    john

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/react-login")
+  .connect("mongodb://127.0.0.1:27017/react-login")
   .then(() => {
     console.log("mongodb connected");
   })
