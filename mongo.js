430d0da5ffa3d65f8a7b66ff434000e7128a1361
johninfant021 <127079706+johninfant021@users.noreply.github.com>
2024-02-15 09:24:37 +0530

    second

---
+++ mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://0.0.0.0:27017/react-login")
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch(() => {
+    console.log("failed");
+  });
+
+//mongodb://localhost:27017/react-login-tut
+
+const newSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const collection = mongoose.model("collection", newSchema);
+
+module.exports = collection;
